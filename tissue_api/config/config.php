53e4cbc869019ba1f88d87ae19a6816f1a959434
VagabondShaman <szaszbalazs94@gmail.com>
2020-06-22 16:14:08 +0200

    Added database connection string

---
+++ tissue_api/config/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bazsi
+ * Date: 2020. 06. 21.
+ * Time: 11:10
+ */
+$server = "localhost";
+$db_user = "printfie_tissue";
+$db_password = "tissuetissue";
+$database = "printfie_tissue";
